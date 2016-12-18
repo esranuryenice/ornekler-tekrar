f375c7dbdaea28b14e2ca3289b3e7587f78ad35d
esran <esran@DESKTOP-O32GGRA>
2016-12-18 15:17:48 +0300

    fakto

---
+++ ikinci-ornek/ikinci-ornek/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ikinci_ornek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Sayıyı Girin :");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            int sayac = 1;
+            int fakto = 1;
+            while (sayac<=sayi)
+            {
+                fakto = fakto * sayac;
+                sayac++;
+            }
+            Console.WriteLine("Sonuc="+fakto);
+            Console.ReadKey();
+        }
+    }
+}
