292958135bc8082c8e598fc25f8a409b123c2b8d
esran <esran@DESKTOP-O32GGRA>
2016-12-18 15:12:21 +0300

    iki sayiyi toplayan ve işlem seçtiren soru

---
+++ birinci-örnek/birinci-örnek/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace birinci_örnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool devam = true ;
+            
+            while (devam)
+            {
+                Console.Write("Birinci Sayi :");
+                Double s1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("İkinci Sayi : ");
+                Double s2 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("İşlem:");
+                char secim = Convert.ToChar(Console.ReadLine());
+                double sonuc = 0;
+                switch (secim)
+                {
+                    case '+':
+                        sonuc = s1 + s2;
+                        break;
+                    case '-':
+                        sonuc = s1 - s2;
+                        break;
+                    case '*':
+                        sonuc = s1 * s2;
+                        break;
+                    case '/':
+                        sonuc = s1 / s2;
+                        break;
+
+                    default:
+                        Console.WriteLine("Böyle Bir İşlem Yok !!!"); 
+                        break;
+                }
+                Console.WriteLine("Sonuç : "+sonuc);
+                Console.WriteLine("Devam Edelim Mi ?(evet-hayır)");
+                string cevap = Console.ReadLine();
+                if (cevap=="evet")
+                {
+                    devam = true;
+                }
+                else
+                {
+                    Console.WriteLine("Görüşmek Üzere!!..");
+                    devam = false;
+                }
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //bool devam = true;
+            //while (devam)
+            //{
+            //    Console.Write("Birinci Sayı : ");
+            //    double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+            //    Console.Write("İkinci Sayı : ");
+            //    double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            //    Console.Write("İşlem Seç : ");
+            //    char islem = Convert.ToChar(Console.ReadLine());
+
+            //    double sonuc = 0;
+            //    switch (islem)
+            //    {
+            //        case '+':
+            //            sonuc = sayi1 + sayi2;
+            //            break;
+            //        case '-':
+            //            sonuc = sayi1 - sayi2;
+            //            break;
+            //        case '*':
+            //            sonuc = sayi1 * sayi2;
+            //            break;
+            //        case '/':
+            //            sonuc = sayi1 / sayi2;
+            //            break;
+            //        default:
+            //            Console.WriteLine("Böyle Bir işlem tipi yok");
+            //            break;
+            //    }
+            //    Console.WriteLine("Sonuc = " + sonuc);
+            //    Console.WriteLine("Devam Edelim Mi?(Evet) ");
+            //    string cevap = Console.ReadLine();
+            //    if (cevap == "evet")
+            //    {
+
+            //        devam = true;
+
+            //    }
+            //    else
+            //    {
+            //        devam = false;
+            //    }
+            //}
+            //Console.WriteLine("Görüşmek Üzere");
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ birinci-örnek/birinci-örnek/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("birinci-örnek")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("birinci-örnek")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d31d5d5e-548b-42c7-9d33-0d4cd1bfbc58")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
