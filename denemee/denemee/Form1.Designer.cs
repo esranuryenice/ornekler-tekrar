a39d78cfea94a2fad665e7fa98a48ec6e28e1b1a
esran <esran@DESKTOP-O32GGRA>
2016-12-18 19:04:53 +0300

    sebzelerin tümü

--- denemee/denemee/Form1.Designer.cs
+++ denemee/denemee/Form1.Designer.cs
@@ -45,8 +45,8 @@
             this.txt_kilo = new System.Windows.Forms.TextBox();
             this.txt_adet = new System.Windows.Forms.TextBox();
             this.txt_gram = new System.Windows.Forms.TextBox();
-            this.btn_hesapla = new System.Windows.Forms.Button();
-            this.lbl_hesapla = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_toplam = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btn_domates
@@ -67,6 +67,7 @@
             this.btn_biber.TabIndex = 1;
             this.btn_biber.Text = "Biber";
             this.btn_biber.UseVisualStyleBackColor = true;
+            this.btn_biber.Click += new System.EventHandler(this.btn_biber_Click);
             // 
             // btn_patlican
             // 
@@ -76,6 +77,7 @@
             this.btn_patlican.TabIndex = 2;
             this.btn_patlican.Text = "Patlıcan";
             this.btn_patlican.UseVisualStyleBackColor = true;
+            this.btn_patlican.Click += new System.EventHandler(this.btn_patlican_Click);
             // 
             // btn_patates
             // 
@@ -85,6 +87,7 @@
             this.btn_patates.TabIndex = 5;
             this.btn_patates.Text = "Patates";
             this.btn_patates.UseVisualStyleBackColor = true;
+            this.btn_patates.Click += new System.EventHandler(this.btn_patates_Click);
             // 
             // btn_maydonoz
             // 
@@ -94,6 +97,7 @@
             this.btn_maydonoz.TabIndex = 4;
             this.btn_maydonoz.Text = "Maydonoz";
             this.btn_maydonoz.UseVisualStyleBackColor = true;
+            this.btn_maydonoz.Click += new System.EventHandler(this.btn_maydonoz_Click);
             // 
             // btn_dereotu
             // 
@@ -103,6 +107,7 @@
             this.btn_dereotu.TabIndex = 3;
             this.btn_dereotu.Text = "Dereotu";
             this.btn_dereotu.UseVisualStyleBackColor = true;
+            this.btn_dereotu.Click += new System.EventHandler(this.btn_dereotu_Click);
             // 
             // btn_karnabahar
             // 
@@ -112,6 +117,7 @@
             this.btn_karnabahar.TabIndex = 8;
             this.btn_karnabahar.Text = "Karnabahar";
             this.btn_karnabahar.UseVisualStyleBackColor = true;
+            this.btn_karnabahar.Click += new System.EventHandler(this.btn_karnabahar_Click);
             // 
             // btn_brokoli
             // 
@@ -121,6 +127,7 @@
             this.btn_brokoli.TabIndex = 7;
             this.btn_brokoli.Text = "Brokoli";
             this.btn_brokoli.UseVisualStyleBackColor = true;
+            this.btn_brokoli.Click += new System.EventHandler(this.btn_brokoli_Click);
             // 
             // btn_sogan
             // 
@@ -130,6 +137,7 @@
             this.btn_sogan.TabIndex = 6;
             this.btn_sogan.Text = "Soğan";
             this.btn_sogan.UseVisualStyleBackColor = true;
+            this.btn_sogan.Click += new System.EventHandler(this.btn_sogan_Click);
             // 
             // lbl_fiyat
             // 
@@ -195,31 +203,29 @@
             this.txt_gram.Size = new System.Drawing.Size(100, 20);
             this.txt_gram.TabIndex = 16;
             // 
-            // btn_hesapla
+            // label1
             // 
-            this.btn_hesapla.Location = new System.Drawing.Point(405, 168);
-            this.btn_hesapla.Name = "btn_hesapla";
-            this.btn_hesapla.Size = new System.Drawing.Size(163, 23);
-            this.btn_hesapla.TabIndex = 17;
-            this.btn_hesapla.Text = "Hesapla";
-            this.btn_hesapla.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(379, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Toplam :";
             // 
-            // lbl_hesapla
+            // txt_toplam
             // 
-            this.lbl_hesapla.AutoSize = true;
-            this.lbl_hesapla.Location = new System.Drawing.Point(405, 196);
-            this.lbl_hesapla.Name = "lbl_hesapla";
-            this.lbl_hesapla.Size = new System.Drawing.Size(35, 13);
-            this.lbl_hesapla.TabIndex = 18;
-            this.lbl_hesapla.Text = "label1";
+            this.txt_toplam.Location = new System.Drawing.Point(438, 173);
+            this.txt_toplam.Name = "txt_toplam";
+            this.txt_toplam.Size = new System.Drawing.Size(100, 20);
+            this.txt_toplam.TabIndex = 18;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(636, 457);
-            this.Controls.Add(this.lbl_hesapla);
-            this.Controls.Add(this.btn_hesapla);
+            this.Controls.Add(this.txt_toplam);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txt_gram);
             this.Controls.Add(this.txt_adet);
             this.Controls.Add(this.txt_kilo);
@@ -239,6 +245,7 @@
             this.Controls.Add(this.btn_domates);
             this.Name = "Form1";
             this.Text = "Sebzeler";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -263,8 +270,8 @@
         private System.Windows.Forms.TextBox txt_kilo;
         private System.Windows.Forms.TextBox txt_adet;
         private System.Windows.Forms.TextBox txt_gram;
-        private System.Windows.Forms.Button btn_hesapla;
-        private System.Windows.Forms.Label lbl_hesapla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_toplam;
     }
 }
 
--- denemee/denemee/Form1.cs
+++ denemee/denemee/Form1.cs
@@ -17,23 +17,161 @@ namespace denemee
             InitializeComponent();
         }
 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<double> tutar = new List<double>();
+        }
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button secilensebze = sender as Button;
+            txt_toplam.Text = secilensebze.Text;
+
+
+           
+            
+            
+
+
+        }
+
         private void btn_domates_Click(object sender, EventArgs e)
         {
-
-            double sonuc = 1;
-            double sayi1, sayi2=2.5;
-            double hesapla;
-            txt_fiyat.Text = "2,5";
-            sayi2 = Convert.ToDouble(txt_fiyat.Text);
             txt_adet.Visible = false;
-            sayi1 = Convert.ToDouble(txt_kilo.Text);
-            if (sayi1>=1)
+            txt_gram.Visible = false;
+            txt_fiyat.Text = "2.5 TL";
+            double sonuc = 0, sayi1 = 2.5, sayi2;
+            sayi2 = Convert.ToDouble(txt_kilo.Text);
+            if (sayi2>=1)
             {
                 sonuc = sayi1 * sayi2;
+                txt_toplam.Text = sonuc.ToString();
             }
-            
-            MessageBox.Show(sonuc.ToString());
-            
+            txt_toplam.Text = sonuc.ToString()+ " TL ";
+
+        }
+
+        private void btn_biber_Click(object sender, EventArgs e)
+        {
+            txt_adet.Visible = false;
+            txt_gram.Visible = false;
+            txt_fiyat.Text = "3 TL";
+            double sonuc = 0, sayi1 = 3, sayi2;
+            sayi2 = Convert.ToDouble(txt_kilo.Text);
+            if (sayi2 >= 1)
+            {
+                sonuc = sayi1 * sayi2;
+                txt_toplam.Text = sonuc.ToString();
+            }
+            txt_toplam.Text = sonuc.ToString() + " TL ";
+
+        }
+
+        private void btn_patlican_Click(object sender, EventArgs e)
+        {
+            txt_adet.Visible = false;
+            txt_gram.Visible = false;
+            txt_fiyat.Text = "2.5 TL";
+            double sonuc = 0, sayi1 = 2.5, sayi2;
+            sayi2 = Convert.ToDouble(txt_kilo.Text);
+            if (sayi2 >= 1)
+            {
+                sonuc = sayi1 * sayi2;
+                txt_toplam.Text = sonuc.ToString();
+            }
+            txt_toplam.Text = sonuc.ToString() + " TL ";
+
+        }
+
+        private void btn_dereotu_Click(object sender, EventArgs e)
+        {
+            txt_kilo.Visible = false;
+            txt_gram.Visible = false;
+            txt_fiyat.Text = "0.50 TL";
+            double sonuc = 0, sayi1 = 0.50, sayi2;
+            sayi2 = Convert.ToDouble(txt_adet.Text);
+            if (sayi2 <= 50)
+            {
+                sonuc = sayi1 * sayi2;
+                txt_toplam.Text = sonuc.ToString();
+            }
+            txt_toplam.Text = sonuc.ToString() + " TL ";
+
+        }
+
+        private void btn_maydonoz_Click(object sender, EventArgs e)
+        {
+            txt_kilo.Visible = false;
+            txt_gram.Visible = false;
+            txt_fiyat.Text = "0.50 TL";
+            double sonuc = 0, sayi1 = 0.50, sayi2;
+            sayi2 = Convert.ToDouble(txt_adet.Text);
+            if (sayi2 <= 50)
+            {
+                sonuc = sayi1 * sayi2;
+                txt_toplam.Text = sonuc.ToString();
+            }
+            txt_toplam.Text = sonuc.ToString() + " TL ";
+        }
+
+        private void btn_patates_Click(object sender, EventArgs e)
+        {
+            txt_adet.Visible = false;
+            txt_gram.Visible = false;
+            txt_fiyat.Text = "2.5 TL";
+            double sonuc = 0, sayi1 = 2.5, sayi2;
+            sayi2 = Convert.ToDouble(txt_kilo.Text);
+            if (sayi2 >= 1)
+            {
+                sonuc = sayi1 * sayi2;
+                txt_toplam.Text = sonuc.ToString();
+            }
+            txt_toplam.Text = sonuc.ToString() + " TL ";
+        }
+
+        private void btn_sogan_Click(object sender, EventArgs e)
+        {
+            txt_adet.Visible = false;
+            txt_gram.Visible = false;
+            txt_fiyat.Text = "2.5 TL";
+            double sonuc = 0, sayi1 = 2.5, sayi2;
+            sayi2 = Convert.ToDouble(txt_kilo.Text);
+            if (sayi2 >= 1)
+            {
+                sonuc = sayi1 * sayi2;
+                txt_toplam.Text = sonuc.ToString();
+            }
+            txt_toplam.Text = sonuc.ToString() + " TL ";
+        }
+
+        private void btn_brokoli_Click(object sender, EventArgs e)
+        {
+            txt_adet.Visible = false;
+            txt_gram.Visible = false;
+            txt_fiyat.Text = "2.5 TL";
+            double sonuc = 0, sayi1 = 2.5, sayi2;
+            sayi2 = Convert.ToDouble(txt_kilo.Text);
+            if (sayi2 >= 1)
+            {
+                sonuc = sayi1 * sayi2;
+                txt_toplam.Text = sonuc.ToString();
+            }
+            txt_toplam.Text = sonuc.ToString() + " TL ";
+        }
+
+        private void btn_karnabahar_Click(object sender, EventArgs e)
+        {
+            txt_adet.Visible = false;
+            txt_gram.Visible = false;
+            txt_fiyat.Text = "2.5 TL";
+            double sonuc = 0, sayi1 = 2.5, sayi2;
+            sayi2 = Convert.ToDouble(txt_kilo.Text);
+            if (sayi2 >= 1)
+            {
+                sonuc = sayi1 * sayi2;
+                txt_toplam.Text = sonuc.ToString();
+            }
+            txt_toplam.Text = sonuc.ToString() + " TL ";
         }
     }
 }
